f7220fb5355df326b068914903d59fb330f9008d
Joe-houghton <Joe_houghton@live.co.uk>
2018-06-24 10:50:12 +0100

    Initial Coding. Config not yet read in

--- PBDomoticz.py
+++ PBDomoticz.py
@@ -6,8 +6,7 @@ import requests
 
 from pushbullet import Listener
 from pushbullet import Pushbullet
-from collections import namedtuple
-
+from domoticz import Domoticz
 
 # CONFIG START
 
@@ -21,79 +20,12 @@ DOMOTICZ_ADDRESS = 'http://192.168.0.1:8080'
 
 #CONFIG END
 
-DomoticzDevice = namedtuple("DomoticzDevice", "name idx type")
-devicesAndScenes = []
+domoticz = None
 pb = None
-
-def populateUsingURL(url, deviceType):
-    requestUrl = DOMOTICZ_ADDRESS + url
-    response = requests.get(requestUrl, auth=(DOMOTICZ_USER, DOMOTICZ_PASS))
-    if response.status_code != 200:
-        print("Bad Request")
-        return None
-    jsonDevices = response.json()
-    if "result" in jsonDevices:
-        devices = jsonDevices["result"]
-        for device in devices:
-            tempDevice = DomoticzDevice(device["Name"], device["idx"], deviceType)
-            devicesAndScenes.append(tempDevice)
-
-def populateDevicesAndScenes():
-    devicesAndScenes = []
-    getAllSwitches = "/json.htm?type=command&param=getlightswitches"
-    populateUsingURL(getAllSwitches, 0)
-    getAllScenes = "/json.htm?type=scenes"
-    populateUsingURL(getAllScenes, 1)
-
-def doesDeviceExist(deviceName):
-    deviceNameToTest = deviceName.lower()
-    for device in devicesAndScenes:
-        if device[0].lower() == deviceNameToTest :
-            return device
-    return None
-
-def getTargetDevice(words):
-    print("finding device...")
-    wordsLength = len(words)
-    targetDevice = ""
-    for i in range(wordsLength - 2):
-        if i > 0 :
-            targetDevice += " "
-        targetDevice += words[i + 2]
-        returnDevice = doesDeviceExist(targetDevice)
-        if returnDevice != None :
-            return returnDevice
-
-    print("No matches for " + targetDevice)
-    return None # No matches
-
-
-def sendCommand(command, deviceId, deviceType):
-    # e.g. '/json.htm?type=command&param=switchscene&idx=1'
-    url = ""
-    param = ""
-    
-    if deviceType == 0:
-        param = "switchlight"
-    elif deviceType == 1:
-        param = "switchscene"
-
-    jsonString = '/json.htm?type=command&'
-    switchCommand = '&switchcmd='
-    seq = (DOMOTICZ_ADDRESS, jsonString, "param=", param, "&idx=", deviceId, switchCommand, command)
-    blankString = ''
-    url = blankString.join(seq)
-
-    print(url, '\n')
-    response = requests.get(url, auth=(DOMOTICZ_USER, DOMOTICZ_PASS))
-    if response.status_code != 200:
-        print("Bad Send Request")
-        return None
-
 
 def getNewPushes():
     pushes = pb.get_pushes()
-    populateDevicesAndScenes()
+    domoticz.populateDevicesAndScenes()
     for push in pushes:
         if not isinstance(push,dict): 
             # not a push object
@@ -101,26 +33,7 @@ def getNewPushes():
         print(push.get('title', ''))
         message = push.get('body', '')
         print(message)
-        lines = message.split('\n')
-        deletePush = False
-        for line in lines:
-            words = line.split(' ')
-            if len(words) > 2:
-                if words[0] == '#command':
-                    deletePush = True
-                    sendCommand(words[1], words[2], 0)
-                elif words[0] == '#commandToScene':
-                    deletePush = True
-                    sendCommand(words[1], words[2], 1)
-                elif words[0] == '#commandByName':
-                    print("Processing Command by Name")
-                    deletePush = True
-                    targetDevice = getTargetDevice(words)
-                    if targetDevice != None:
-                        print("Target Device is " + targetDevice[0])
-                        sendCommand(words[1], targetDevice[1], targetDevice[2])
-                    else:
-                        print("Cannot find device: ")
+        deletePush = domoticz.ProcessCommand(message)
         if deletePush:    
             pb.dismiss_push(push.get('iden'))
             pb.delete_push(push.get("iden"))
@@ -143,6 +56,13 @@ def main():
                  http_proxy_host=HTTP_PROXY_HOST,
                  http_proxy_port=HTTP_PROXY_PORT)
     print('Listening for pushes')
+    
+    
+    domoticz = new Domoticz(DOMOTICZ_ADDRESS, DOMOTICZ_USER, DOMOTICZ_PATH)
+    if domoticz == None:
+        print('Cannot Initialise Domoticz, Check Configuration')
+        exit()
+    
     pb.delete_pushes() #lets delete any pushes  that we already have so they do not trigger now
     try:
         s.run_forever()
